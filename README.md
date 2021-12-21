8ff7ae213fe368216adc1a98986de5bbb490fbae
NotAName320 <rc144@outlook.com>
2021-12-21 12:00:00 -0500

    add readme and make sure all the licenses are up to snuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# fake_hockey_bot
+A bot for the Fake Hockey Discord Server. Elements borrowed from the Fake Soccer Bot, also written by me. Licensed under MIT.
+
+# Installation Instructions
+Coming soon when bot is stable
--- fake_hockey_bot.py
+++ fake_hockey_bot.py
@@ -1,6 +1,8 @@
 """
-Bot to simulate a soccer game using the principles of number guessing
+Bot to simulate a hockey game using the principles of number guessing
+
 Copyright (c) 2021 NotAName
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -9,6 +11,7 @@ copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
